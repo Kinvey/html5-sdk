66009813beb40a65faa12ffa33b4c367b247a13c
Thomas Conner <thomas@kinvey.com>
2016-11-14 11:28:32 -0500

    Update scripts/e2e.js to copy SDK build

--- scripts/e2e.js
+++ scripts/e2e.js
@@ -1,18 +1,23 @@
 /* eslint-disable */
-var fs = require('fs');
+var fs = require('fs-extra')
 var path = require('path');
-var IsThere = require("is-there");
 var e2eAppPath = path.resolve(__dirname, '../test/browserstack/app');
 var e2eAppConfigFile = path.resolve(__dirname, '../test/browserstack/kinvey.json');
 
 // Make e2e app directory if it doesn't exist
-IsThere(e2eAppPath, function(exists) {
-  if (exists === false) {
-    fs.mkdir(e2eAppPath, function() {
-      if (exec('cd ' + e2eAppPath + ' && yo kinvey-html --config ' + e2eAppConfigFile).code !== 0) {
-        echo('Error: Git commit failed');
-        exit(1);
-      }
-    });
+fs.emptyDir(e2eAppPath, function (error) {
+  if (error) {
+    echo('An error occurred.');
+    exit(1);
   }
+
+  if (exec('cd ' + e2eAppPath + ' && yo kinvey-html --config ' + e2eAppConfigFile).code !== 0) {
+    echo('An error occurred.');
+    exit(1);
+  }
+
+  cp(
+    path.resolve(__dirname, '../dist/kinvey-html5-sdk.js'),
+    path.resolve(__dirname, '../test/browserstack/app/static/bower_components/kinvey-html5-sdk/dist/kinvey-html5-sdk.js')
+  );
 });
