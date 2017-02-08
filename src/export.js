c0879a58ad31b852901874797f13d2cd9bd3a619
Thomas Conner <thomas@kinvey.com>
2017-02-08 15:22:43 -0500

    Remove src/export.js

--- src/export.js
+++
@@ -1,9 +0,0 @@
-import Kinvey from './kinvey';
-import Popup from './popup';
-
-export * from 'kinvey-node-sdk/dist/export';
-export * from './middleware';
-export { Popup };
-
-// Export default
-export default Kinvey;
