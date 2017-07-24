6ef87f0334d505998352b89d7361c1169f8a8345
Thomas Conner <thomas@kinvey.com>
2017-07-24 14:21:48 -0400

    Import KinveyStorage properly

--- src/middleware/src/storage/index.js
+++ src/middleware/src/storage/index.js
@@ -1,5 +1,5 @@
 import { isDefined } from 'kinvey-js-sdk/dist/export';
-import KinveyStorage from 'kinvey-js-sdk/dist/request/src/middleware/src/storage';
+import { Storage as KinveyStorage } from 'kinvey-js-sdk/dist/request/src/middleware/src/storage';
 import { IndexedDBAdapter } from './indexeddb';
 import { WebSQLAdapter } from './websql';
 import { LocalStorageAdapter } from './webstorage';
