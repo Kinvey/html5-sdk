6658c8c98a1152f6f9c9cd8fe6b2bcdba80051ce
Thomas Conner <thomas@kinvey.com>
2017-08-03 16:28:55 -0400

    Update bundle.js

--- src/bundle.js
+++ src/bundle.js
@@ -1,15 +1,21 @@
-import CoreKinvey from 'kinvey-js-sdk/dist/bundle';
+import * as CoreKinvey from 'kinvey-js-sdk';
 import { User } from './user';
 import { Kinvey } from './kinvey';
+import { Client } from './client';
 
 // Add all existing namespaces
 const keys = Object.keys(CoreKinvey);
 keys.forEach((key) => {
-  Kinvey[key] = CoreKinvey[key];
+  if (key !== 'Kinvey') {
+    Kinvey[key] = CoreKinvey[key];
+  }
 });
 
-// Replace User Class
+// Replace User class
 Kinvey.User = User;
+
+// Replace Client class
+Kinvey.Client = Client;
 
 // Export
 module.exports = Kinvey;
